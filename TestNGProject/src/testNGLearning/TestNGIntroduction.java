ecd3e2b128f26f3b280aed59e4c585314e0752f6
surendar <surendarg97@gmail.com>
2020-07-10 08:14:39 +0530

    TestNG automation

---
+++ TestNGProject/src/testNGLearning/TestNGIntroduction.java
@@ -0,0 +1,13 @@
+package testNGLearning;
+
+import org.testng.annotations.Test;
+
+public class TestNGIntroduction {
+	
+	@Test
+	public void introduction() {
+		
+		System.out.println("Hello world");
+	}
+
+}
