ff0e8ae8b36ae697fd5770c0154ed9181dddec57
surendar <surendarg97@gmail.com>
2020-07-10 07:50:01 +0530

    Github commit check

---
+++ BankingApplication/src/ScannerTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+	
+	public static void main(String args[]) {
+		
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		
+		int value = scanner.nextInt();
+		
+		System.out.println("Value is "+ value);
+		
+		System.out.println("Enter a character");
+		
+		char charAt = scanner.next().charAt(0);
+		
+		System.out.println("Character is "+ charAt);
+		
+		
+		
+		
+	}
+
+}
