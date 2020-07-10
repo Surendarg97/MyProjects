c65663992cede6636e658f5f6f8d7b34f3271a8e
surendar <surendarg97@gmail.com>
2020-07-10 08:13:09 +0530

    Framework designing project

---
+++ FrameworkProject/src/test/java/TestCases/ChromeBrowserInvoke.java
@@ -0,0 +1,44 @@
+package TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeBrowserInvoke {
+	
+	@Test
+
+	public static void newChromeBrowserInvoke() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"E:\\Files\\Selenium learning from udemy course\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+
+		/*
+		 * driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+		 * driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		 * driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+		 */
+
+		driver.manage().window().maximize();
+
+		System.out.println(driver.getTitle());
+
+		System.out.println(driver.getCurrentUrl());
+
+		// System.out.println(driver.getPageSource());
+
+		//driver.get("http://www.yahoo.com/");
+
+		//driver.navigate().back();
+
+//		driver.navigate().forward();
+
+		//driver.quit();
+
+	}
+
+}
---
+++ FrameworkProject/src/test/java/TestCases/Facebook.java
@@ -0,0 +1,37 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Facebook {
+
+	@Test
+	public static void newFacebook() {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Files\\Selenium learning from udemy course\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("surendarg97@gmail.com");
+		
+		driver.findElement(By.name("pass")).sendKeys("6796g8530");
+		
+		driver.findElement(By.id("loginbutton")).click();
+		
+		/*
+		 * driver.findElement(By.linkText("Forgotten account?")).click();
+		 * 
+		 * driver.quit();
+		 */		
+		
+
+	}
+
+}
